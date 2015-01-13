fe9042858ef55e4bd91b29c96ec0cecc0732a495
EthanH <l2weekly.cn@gmail.com>
2015-01-13 13:20:27 +0800

    add angular

--- app/scripts/angular/app.js
+++ app/scripts/angular/app.js
@@ -6,3 +6,13 @@
  * Email: l2weekly.cn@gmail.com
  * Date: 2015-01-12
  */
+'use strict';
+
+var app = angular.module('app', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/', {templateUrl: '/views/templates/welcome.html', controller: 'WelcomeCtrl'})
+		.otherwise({redirectTo: '/'});
+	$locationProvider.html5Mode(true);
+}]);
--- app/scripts/angular/controllers.js
+++ app/scripts/angular/controllers.js
@@ -6,3 +6,8 @@
  * Email: l2weekly.cn@gmail.com
  * Date: 2015-01-12
  */
+'use strict';
+
+function WelcomeCtrl($scope) {
+	$scope.username = 'Conan_Z';
+}
